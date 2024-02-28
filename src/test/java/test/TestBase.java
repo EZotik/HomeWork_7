d662ced85ef7fd44887001462071f131242d6373
Ekaterina Zotova <karmengerl@gmail.com>
2024-02-28 11:30:03 +0300

    commit with Driver config

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,16 +1,22 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.DriverConfig;
 import helpers.Attach;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static io.qameta.allure.Allure.step;
@@ -19,21 +25,23 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        String browser = System.getProperty("browser", "chrome");
-        String browserVersion = System.getProperty("browser_version", "100.0");
-        String browserSize = System.getProperty("browser_size", "1920x1080");
-        String remoteUrl = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        String browserConfig = String.format("Browser: %s, version: %s, window size: %s, remoteUrl: %s",
-                browser, browserVersion, browserSize, remoteUrl);
-        System.out.println(browserConfig);
-        Configuration.browser = browser;
-        Configuration.browserVersion = browserVersion;
-        Configuration.browserSize = browserSize;
+        DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
+
+        Configuration.browser = driverConfig.browserName();
+        Configuration.browserVersion = driverConfig.browserVersion();
+        Configuration.browserSize = driverConfig.browserSize();
+        Configuration.remote = driverConfig.browserUrl();
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "normal";
         Configuration.holdBrowserOpen = false;
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.remote = remoteUrl;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
@@ -42,20 +50,8 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        Selenide.closeWebDriver();
 
-    }
-
-    @AfterAll
-    static void terminateWebDriver() {
-        step("Закрываем страницу браузера", () -> {
-            closeWebDriver();
-        });
-
-    }
-
-    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
-    public byte[] takeScreenshot(){
-        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
 }
