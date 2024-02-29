7c1c603e7e08124ec0196722053293fcc40926f7
Ekaterina Zotova <karmengerl@gmail.com>
2024-02-29 19:48:14 +0300

    Splitting Tests

--- src/test/java/test/TestRegistrationWithRandomUtils.java
+++ src/test/java/test/TestRegistrationWithRandomUtils.java
@@ -10,6 +10,7 @@ import utils.RandomUtils;
 import static io.qameta.allure.Allure.step;
 
 
+@Tag("practiceform")
 public class TestRegistrationWithRandomUtils extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
@@ -29,7 +30,6 @@ public class TestRegistrationWithRandomUtils extends TestBase {
     String invalidPhoneNumber = " ";
 
     @Test
-    @Tag("practiceform")
     @Feature("Форма Practice Form")
     @DisplayName("Заполнение формы Practice Form")
     void fillFormTest() {
@@ -85,67 +85,8 @@ public class TestRegistrationWithRandomUtils extends TestBase {
                     .checkResult(fieldStateCity, random.state + " " + random.city);
         });
     }
-    @Test
-    @Tag("simple")
-    @Feature("Форма Practice Form")
-    @DisplayName("Заполнение формы Practice Form с использованием First Name и Last Name из properties")
-    void fillFormRegistrationTest() {
-        step("Открываем главную страницу", () -> {
-            registrationPage.openPage();
-        });
-        step("Указываем First Name", () -> {
-            registrationTest.setFirstName();
-        });
-        step("Указываем Last Name", () -> {
-            registrationTest.setLastName();
-        });
-        step("Указываем Email", () -> {
-            registrationPage.setEmail(random.email);
-        });
-        step("Указываем Gender", () -> {
-            registrationPage.setGender(random.gender);
-        });
-        step("Указываем Phone Number", () -> {
-            registrationPage.setUserNumber(random.phoneNumber);
-        });
-        step("Указываем Date of Birth", () -> {
-            registrationPage.setDateOfBirth(random.dayOfBirth, random.monthOfBirth, random.yearOfBirth);
-        });
-        step("Указываем Subjects", () -> {
-            registrationPage.setSubjects(random.subject);
-        });
-        step("Указываем Hobbies", () -> {
-            registrationPage.setHobbies(random.hobby);
-        });
-        step("Прикрепляем Picture", () -> {
-            registrationPage.setPicture(file);
-        });
-        step("Указываем Current Address", () -> {
-            registrationPage.setUserAddress(random.address);
-        });
-        step("Указываем State and City", () -> {
-            registrationPage.setStateAndCity(random.state, random.city);
-        });
-        step("Кликаем по кнопке Submit", () -> {
-            registrationPage.submit();
-        });
-        step("Проверяем поля в итоговой таблице", () -> {
-            registrationPage.checkResult(fieldName, registrationTest.firstName + " " +
-                            registrationTest.lastName)
-                    .checkResult(fieldEmail, random.email)
-                    .checkResult(fieldGender, random.gender)
-                    .checkResult(fieldMobile, random.phoneNumber)
-                    .checkResult(fieldBirthday, random.dayOfBirth + " " + random.monthOfBirth + "," + random.yearOfBirth)
-                    .checkResult(fieldSubjects, random.subject)
-                    .checkResult(fieldHobbies, random.hobby)
-                    .checkResult(fieldPicture, file)
-                    .checkResult(fieldАddress, random.address)
-                    .checkResult(fieldStateCity, random.state + " " + random.city);
-        });
-    }
 
     @Test
-    @Tag("practiceform")
     @Feature("Форма Practice Form")
     @DisplayName("Заполнение формы Practice Form без полей Subjects, Hobbies, Picture, State and City")
     void minDataFillFormTest() {
@@ -185,9 +126,7 @@ public class TestRegistrationWithRandomUtils extends TestBase {
                     .checkResult(fieldАddress, random.address);
         });
     }
-
     @Test
-    @Tag("practiceform")
     @Feature("Форма Practice Form")
     @DisplayName("Заполнение формы Practice Form с невалидынм Phone Number")
     void negativeFillFormTest() {
