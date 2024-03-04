ee0134c5bc7f3076ab9a5a48e8bdcc746e0eaf69
Ekaterina Zotova <karmengerl@gmail.com>
2024-03-04 13:07:02 +0300

    Changes for selecting data from properties

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -2,24 +2,16 @@ package test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.DriverConfig;
 import helpers.Attach;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static io.qameta.allure.Allure.step;
 
 public class TestBase {
 
